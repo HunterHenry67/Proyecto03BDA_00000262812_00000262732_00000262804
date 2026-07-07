73fbf0f6c7799c7bd4baf7fa7e87a7f2269c7943
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-07-07 00:23:44 -0700

    Creación de Esqueleto de Proyecto 03

---
+++ Proyecto03BDDA_00000262812_00000262732_00000262804/src/main/java/enriquemadridalvarez/proyecto03bdda_00000262812_00000262732_00000262804/Proyecto03BDDA_00000262812_00000262732_00000262804.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package enriquemadridalvarez.proyecto03bdda_00000262812_00000262732_00000262804;
+
+/**
+ *
+ * @author BALAMRUSH
+ */
+public class Proyecto03BDDA_00000262812_00000262732_00000262804 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
